d6b0b2f6d13b649a8c09536bce46bff2722fb8b7
jessicagomes2205 <jessicagomes2205@gmail.com>
2024-06-04 08:04:52 -0300

    testes com seletores

--- cypress/e2e/seletores.cy.js
+++ cypress/e2e/seletores.cy.js
@@ -1,57 +1,62 @@
 ///<reference types = "cypress"/>
 
 describe('Seletores avançados com cypress', () => {
-
   beforeEach(() => {
-    cy.visit('../../seletores.html')
+    cy.visit('../../seletores.html');
   });
 
   it('Seleciona elementos que contêm um Texto específico', () => {
-    //TODO: 
+    cy.contains('Irmão 2').should('exist');
+    cy.contains('Enviar').click();
   });
-    
+
   it('Seleciona o elemento com a classe pai', () => {
-    //TODO: 
-  })
+    cy.get('.pai').should('exist');
+  });
 
   it('Seleciona o elemento com o id Filho', () => {
-    //TODO: 
-   })
+    cy.get('#id-filho').should('have.attr', 'class', 'filho-4');
+  });
 
   it('Seleciona um elemento filho dentro do elemento com a classe pai', () => {
-    //TODO: 
+    cy.get('.pai').find('.filho-4').should('contain', 'Item solto');
   });
 
   it('Seleciona o segundo elemento <span> com a classe irmao', () => {
-    //TODO: 
+    cy.get('.irmao + .irmao').should('contain', 'Irmão 2');
   });
 
   it('Seleciona o próximo elemento irmão', () => {
-    //TODO: 
+    cy.get('.item-especial').next().should('contain', 'Item 3');
   });
 
   it('Seleciona o elemento irmão anterior', () => {
-    //TODO: 
+    cy.get('.item-especial').prev().should('contain', 'Item 1');
   });
 
   it('Seleciona o irmão da div anterior', () => {
-    //TODO: 
+    cy.get('[name="nome-do-atributo"]').prev().should('contain', 'Item 1');
   });
 
   it('Seleciona o terceiro elemento <li> encontrado', () => {
-    //TODO: 
+    cy.get('li').eq(2).should('contain', 'Item 3');
   });
 
   it('Seleciona o elemento com o atributo data-test', () => {
-    //TODO: 
+    cy.get('[data-test="div-pai"]').should('contain', 'Item solto');
   });
 
-  it('Seleciona o elemento com a classe pai do elemento com a classe filho', () => {
-    //TODO: 
-  });
+  it.skip('Seleciona o elemento com a classe pai do elemento com a classe filho', () => {
+    cy.get('.item-especial').parent('.pai').should('have.attr', 'class', 'pai');
+  }); //REVER
 
   it('Seleciona o elemento com um valor em um select', () => {
-    //TODO: 
+    cy.get('select').select('Muito');
+    cy.get('[type="submit"]').click();
+    cy.get('#mensagemFeedback').should(
+      'contain',
+      'Obrigado por compartilhar conosco!'
+    );
+    cy.contains('Obrigado por compartilhar conosco!');
   });
-
-})
+});
